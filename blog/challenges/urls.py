b826626ce4dbce9d43ecb2ae7f029f3054f110d7
WalmirFerJr <walmir.jr.2004@usp.br>
2025-10-17 01:08:12 -0300

    Adicionando restante dos meses

--- blog/challenges/urls.py
+++ blog/challenges/urls.py
@@ -3,6 +3,16 @@ from . import views # importando arquivo views da mesma pasta em que nosso arqui
 
 urlpatterns = [
  path("january", views.januaryView),
- path("february", views.februaryView)
+ path("february", views.februaryView),
+ path("march", views.marchView),
+ path("april", views.aprilView),
+ path("may", views.mayView),
+ path("june", views.juneView),
+ path("july", views.julyView),
+ path("august", views.augustView),
+ path("september", views.septemberView),
+ path("october", views.octoberView),
+ path("november", views.novemberView),
+ path("december", views.decemberView),
 ]
 
--- blog/challenges/views.py
+++ blog/challenges/views.py
@@ -3,10 +3,50 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def januaryView(request):
+def januaryView(request): # Cada view deve ter uma função própria
     
-    return  HttpResponse("Este é o desafio de janeiro!")
+    return  HttpResponse("Em janeiro aproveitei um pouco o fim dos vestibulares para tirar férias e fazer uma viagem")
 
 def februaryView(request):
 
-    return HttpResponse("Este é o desafio de fevereiro!")
+    return HttpResponse("Em Fevereiro estive ansioso em relação ao meu ingresso na USP")
+
+def marchView(request):
+
+    return HttpResponse("Em março...")
+
+def aprilView(request):
+
+    return HttpResponse("Em abril...")
+
+def mayView(request):
+
+    return HttpResponse("Em maio...")
+
+def juneView(request):
+
+    return HttpResponse("Em junho...")
+
+def julyView(request):
+
+    return HttpResponse("Em julho...")
+
+def augustView(request):
+
+    return HttpResponse("Em agosto...")
+
+def septemberView(request):
+
+    return HttpResponse("Em setembro...")
+
+def octoberView(request):
+
+    return HttpResponse("Em outubro...")
+
+def novemberView(request):
+
+    return HttpResponse("Em novembro...")
+
+def decemberView(request):
+
+    return HttpResponse("Em dezembro...")
