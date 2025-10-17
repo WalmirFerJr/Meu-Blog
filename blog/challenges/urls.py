76442d26f503edb1967ff156b0afff439d606556
WalmirFerJr <walmir.jr.2004@usp.br>
2025-10-17 01:38:51 -0300

    revertendo feature de todos meses adicionados de forma manual

--- blog/challenges/urls.py
+++ blog/challenges/urls.py
@@ -3,16 +3,6 @@ from . import views # importando arquivo views da mesma pasta em que nosso arqui
 
 urlpatterns = [
  path("january", views.januaryView),
- path("february", views.februaryView),
- path("march", views.marchView),
- path("april", views.aprilView),
- path("may", views.mayView),
- path("june", views.juneView),
- path("july", views.julyView),
- path("august", views.augustView),
- path("september", views.septemberView),
- path("october", views.octoberView),
- path("november", views.novemberView),
- path("december", views.decemberView),
+ path("february", views.februaryView)
 ]
 
--- blog/challenges/views.py
+++ blog/challenges/views.py
@@ -3,50 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def januaryView(request): # Cada view deve ter uma função própria
+def januaryView(request):
     
-    return  HttpResponse("Em janeiro aproveitei um pouco o fim dos vestibulares para tirar férias e fazer uma viagem")
+    return  HttpResponse("Este é o desafio de janeiro!")
 
 def februaryView(request):
 
-    return HttpResponse("Em Fevereiro estive ansioso em relação ao meu ingresso na USP")
-
-def marchView(request):
-
-    return HttpResponse("Em março...")
-
-def aprilView(request):
-
-    return HttpResponse("Em abril...")
-
-def mayView(request):
-
-    return HttpResponse("Em maio...")
-
-def juneView(request):
-
-    return HttpResponse("Em junho...")
-
-def julyView(request):
-
-    return HttpResponse("Em julho...")
-
-def augustView(request):
-
-    return HttpResponse("Em agosto...")
-
-def septemberView(request):
-
-    return HttpResponse("Em setembro...")
-
-def octoberView(request):
-
-    return HttpResponse("Em outubro...")
-
-def novemberView(request):
-
-    return HttpResponse("Em novembro...")
-
-def decemberView(request):
-
-    return HttpResponse("Em dezembro...")
+    return HttpResponse("Este é o desafio de fevereiro!")
